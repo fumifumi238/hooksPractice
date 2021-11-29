99e1b8f2623cdf0e0635d33c24f292521d9c510e
epthalite <fmy628.1998@icloud.com>
2021-11-29 19:39:57 +0900

    finish useMemo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useRef} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// countの初期値として、1~10までのランダムな数値を生成
 
-class Counter extends React.Component {
-  render() {
-    return (
-      <>
-      </>
-    );
-  }
-}
+const App = () => {
+  const inputEl = useRef(null);
+  const handleClick = () => {
+    inputEl.current.focus();
+    console.log("inputEl.current:", inputEl.current);
+    //inputEl.current: <input type="text">
+  };
+  return (
+    <>
+      <input ref={inputEl} type="text" />
+      <button onClick={handleClick}>入力エリアをフォーカスする</button>
+    </>
+  );
+};
 
-export default Counter;
-ReactDOM.render(<Counter />, document.getElementById("root"));
+// useStateはstateの更新時に再レンダリングする。
+// const App = () => {
+//   const inputEl = useRef(null);
+//   const [text, setText] = useState("");
+//   const handleClick = () => {
+//     setText(inputEl.current.value);
+//   };
+//   console.log("レンダリング！！");
+//   return (
+//     <>
+//       <input ref={inputEl} type="text" />
+//       <button onClick={handleClick}>set text</button>
+//       <p>テキスト : {text}</p>
+//     </>
+//   );
+// };
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
